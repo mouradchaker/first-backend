950761f7e838baf9dd70ac50402d642df8642eb2
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-03-17 23:48:13 +0100

    backend-config

--- Backend/index.js
+++ Backend/index.js
@@ -2,3 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL, () => console.log("Db is connected"));
+
+app.listen(process.env.PORT, () =>
+  console.log("Server has been started successfully")
+);
